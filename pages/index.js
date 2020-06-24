10b48defc24b2d98a51199af5b75eb60478df270
Arthur Borges <arthurborges.dev@gmail.com>
2020-06-23 21:52:51 -0300

    Added logo, global stylesheet, aligned logo and h1 with flexbox, added Orbitron font

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,14 @@ export default function Home() {
   return (
     <>
       <Head>
+        <title>Neverendx - Arthur Borges's never ending portfolio</title>
         <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet" />
       </Head>
-      <h1>neverendx</h1>
-      <p>A journey that never ends</p>
+      <div className="brand">
+        <img src="/images/neverendx-logo.svg" alt="Neverendx Logo" className="logo" />
+        <h1>A journey that never ends</h1>
+      </div>
+      
     </>
   );
 }
