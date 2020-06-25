c0d5064d43c32db31940542da5414a02c363e7ef
Arthur Borges <arthurborges.dev@gmail.com>
2020-06-25 19:25:12 -0300

    Finished logo and logo description. Finished this branch

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,16 @@ export default function Home() {
         <title>Neverendx - Arthur Borges's never ending portfolio</title>
         <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet" />
       </Head>
-      <div className="brand">
-        <img src="/images/neverendx-logo.svg" alt="Neverendx Logo" className="logo" />
+      <div className="col-4">
+
+      </div>
+      <div className="brand col-4">
+        <img src="/images/neverendx-logo-resized.svg" alt="Neverendx Logo" className="logo" />
         <h1>A journey that never ends</h1>
       </div>
-      
+      <div className="col-4">
+
+      </div>
     </>
   );
 }
