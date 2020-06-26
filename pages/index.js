a8dc320d07451f885c43b99eb674c01fd64ccd2b
Arthur Borges <arthurborges.dev@gmail.com>
2020-06-25 21:28:48 -0300

    Finished content and brand of home page. TODO: Social buttons and next button

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,32 @@ export default function Home() {
         <title>Neverendx - Arthur Borges's never ending portfolio</title>
         <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet" />
       </Head>
-      <div className="col-4">
-
-      </div>
-      <div className="brand col-4">
-        <img src="/images/neverendx-logo-resized.svg" alt="Neverendx Logo" className="logo" />
-        <h1>A journey that never ends</h1>
-      </div>
-      <div className="col-4">
-
-      </div>
+      <main>
+        <section className="brand">
+          <img src="/images/neverendx-logo-resized.svg" alt="Neverendx Logo" className="logo" />
+          <h1>A journey that never ends</h1>
+        </section>
+        <section className="content">
+          <div className="profile">
+            <div className="photo-container">
+              <img src="/images/avatar.jpeg" alt="Arthur Borges's profile photo" className="photo" />
+            </div> 
+            <div className="social">
+              <div className="icons-container">
+              
+              </div>
+            </div>  
+          </div>
+          <article className="about">
+            <p>
+              Hello!! My name is Arthur Borges and this is my portfolio that never ends.
+              Here every page is a React Component like this one, developed using Next.js
+              and served by Vercel's platform. Click on the arrow on the right to navigate 
+              through pages. Enjoy! 
+            </p>
+          </article>
+        </section>
+      </main>
     </>
   );
 }
